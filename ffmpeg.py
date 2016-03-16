8536181ccf71e6c4177ec800887996ba6d363450
10se1ucgo <hammaadu1@gmail.com>
2016-03-16 17:16:05 -0400

    OOPS. FFmpeg converter stopped passing the rate value!

--- ffmpeg.py
+++ ffmpeg.py
@@ -348,5 +348,5 @@ def convert_audio(file, dest, rate, vol, codec="pcm_s16le"):
     # cmd = '{ff} -y -i "{i}" -map_metadata -1 -ac 1 -aq 100 -acodec {codec} -ar {rate} -af volume={vol} "{dest}.wav"'
     # cmd = cmd.format(ff=find(), i=file, codec=codec, rate=rate, vol=vol / 100, dest=dest)
     cmd = (find(), '-y', '-i', file, '-map_metadata', '-1', '-ac', '1', '-aq', '100',
-           '-acodec', codec, '-af', 'volume={vol}'.format(vol=vol/100), '{dest}.wav'.format(dest=dest))
+           '-acodec', codec, '-ar', rate, '-af', 'volume={vol}'.format(vol=vol/100), '{dest}.wav'.format(dest=dest))
     return subprocess.check_output(cmd, stderr=subprocess.STDOUT)
